6248e28e7046e66467e90cd5540cb668914a0eca
arthurjoecodes <artniel23@gmail.com>
2021-11-22 19:13:15 +0300

    modified files

---
+++ goodnumbers.py
@@ -0,0 +1,20 @@
+print('hello')
+
+print('what is your name')
+
+first_name = 'Arthur'
+last_name = 'Joe'
+
+input('please enter your name')
+input('please enter another name')
+
+output = 'Hello, {0}, {1}' .format(first_name, last_name)
+output = f'Hello, {first_name} {last_name}' 
+print(output)
+
+first_num = input('please enter a number')
+second_num = input('please enter another number')
+print(int(first_num) + int(second_num))
+
+print('thank you for your time')
+print('bye Arhtur Joe')
